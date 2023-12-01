a5753838d443596829293f955091e5da953ee66b
EV2 <72415130+EV2872@users.noreply.github.com>
2023-12-01 08:35:39 +0000

    Update audioColision.cs

--- audioColision.cs
+++ audioColision.cs
@@ -4,24 +4,18 @@ using UnityEngine;
 
 public class audioColision : MonoBehaviour
 {
-    public AudioClip audioAColisionar;  // El audio que se reproducirá al colisionar
+    public AudioClip audioAColisionar;
     private void OnCollisionEnter(Collision collision) {
-        // Verifica si la colisión involucra al objeto B
         if (collision.gameObject.CompareTag("spider")) {
             ReproducirAudio();
         }
     }
 
     private void ReproducirAudio() {
-        // Verifica si se asignó un audio a reproducir
         if (audioAColisionar != null) {
-            // Crea un nuevo AudioSource temporal para reproducir el audio
             AudioSource audioSource = gameObject.AddComponent<AudioSource>();
-            // Asigna el clip de audio
             audioSource.clip = audioAColisionar;
-            // Reproduce el audio
             audioSource.Play();
-            // Destruye el AudioSource temporal después de reproducir el audio
             Destroy(audioSource, audioAColisionar.length);
         }
     }
